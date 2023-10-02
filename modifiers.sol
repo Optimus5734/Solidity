14ca15abb6e171b34baaca5a265acb78f0de8908
Optimus5734 <sagarverma5754@gmail.com>
2023-10-02 20:19:25 +0530

    modifiers example added

---
+++ modifiers.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
+
+contract testTokens{
+    address public owner;
+    bool public paused;
+    mapping(address=>uint) public balances;
+    
+    constructor(){
+        owner=msg.sender;
+        paused=false;
+        balances[owner]=1000;
+
+    }
+    modifier trueOwner(){
+        require(msg.sender == owner,"Only owner can move further");
+        _;
+    }
+    modifier isPaused(){
+        require(paused == false,"Sorry the contract is paused");
+        _;
+    }    
+    function pause() public trueOwner(){
+        paused=true;
+    }
+    function unpause() public trueOwner(){
+        paused=false;
+    }
+function transfer(address to, uint amount) public isPaused{
+    require(balances[msg.sender]>=amount,"Hy your balance is low");
+    balances[msg.sender]-=amount;
+    balances[to]+=amount;
+}
+
+
+}
