484fdac95a5f3cb4dd0cf29fab70bb42c3a8bae1
Optimus5734 <sagarverma5754@gmail.com>
2023-10-02 20:19:49 +0530

    Done

--- modifiers.sol
+++ modifiers.sol
@@ -1,36 +1,38 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.13;
 
-contract testTokens{
+contract testTokens {
     address public owner;
     bool public paused;
-    mapping(address=>uint) public balances;
-    
-    constructor(){
-        owner=msg.sender;
-        paused=false;
-        balances[owner]=1000;
+    mapping(address => uint) public balances;
 
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
     }
-    modifier trueOwner(){
-        require(msg.sender == owner,"Only owner can move further");
+
+    modifier trueOwner() {
+        require(msg.sender == owner, "Only owner can move further");
         _;
     }
-    modifier isPaused(){
-        require(paused == false,"Sorry the contract is paused");
+    modifier isPaused() {
+        require(paused == false, "Sorry the contract is paused");
         _;
-    }    
-    function pause() public trueOwner(){
-        paused=true;
     }
-    function unpause() public trueOwner(){
-        paused=false;
+
+    function pause() public trueOwner {
+        paused = true;
     }
-function transfer(address to, uint amount) public isPaused{
-    require(balances[msg.sender]>=amount,"Hy your balance is low");
-    balances[msg.sender]-=amount;
-    balances[to]+=amount;
-}
 
+    function unpause() public trueOwner {
+        paused = false;
+    }
 
+    function transfer(address to, uint amount) public isPaused {
+        require(balances[msg.sender] >= amount, "Hy your balance is low");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
 }
+//a basic example of modifier
